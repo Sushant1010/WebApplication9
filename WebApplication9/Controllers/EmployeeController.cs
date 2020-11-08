b1109e6ec86d599a9f336b8748619ddd88c8adce
Sushant Maharjan <sushantmaharjan4545@outlook.com>
2020-11-07 20:26:25 -0800

    Add project files.

---
+++ WebApplication9/Controllers/EmployeeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication9.Models;
+
+namespace WebApplication9.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        //Get
+        //api/Employee
+        //Get
+        //api/Employee/1
+        //Post
+        //api/Employee
+        //PUT
+        //api/Emplolyee
+        //DELETE
+        //api/Employee/1
+
+        EmployeeDB empDB = new EmployeeDB();
+
+        public List<Employee> Get()
+        {
+            return empDB.ListAll();
+        }
+        public void Post(Employee emp)
+        {
+            empDB.Add(emp);
+        }
+        public Employee Get(int ID)
+        {
+            var emp = empDB.ListAll().Find(x => x.EmployeeId.Equals(ID));
+            return emp;
+        }
+        public void Put(Employee emp)
+        {
+            empDB.Update(emp);
+        }
+        public void Delete(int ID)
+        {
+            empDB.Delete(ID);
+        }
+    }
+}
---
+++ WebApplication9/Models/EmployeeDB.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication9.Models;
+
+namespace WebApplication9.Models
+{
+    public class EmployeeDB
+    {
+        TestDBEntities _db = new TestDBEntities();
+        public List<Employee> ListAll()
+        {
+            List<Employee> lstemp = new List<Employee>();
+            var emps = _db.tblEmployees.ToList();
+            foreach (var item in emps)
+            {
+                lstemp.Add(new Employee() { EmployeeId = item.EmployeeID, Name = item.Name, Age = item.Age, Country = item.Country, State = item.State });
+            }
+            return lstemp;
+        }
+
+        //Method for Adding an Employee
+        public int Add(Employee emp)
+        {
+            tblEmployee tb = new tblEmployee();
+            tb.Name = emp.Name;
+            tb.Age = emp.Age;
+            tb.State = emp.State;
+            tb.Country = emp.Country;
+
+            _db.tblEmployees.Add(tb);
+            return _db.SaveChanges();
+        }
+
+        //Method for Updating Employee record
+        public int Update(Employee emp)
+        {
+            tblEmployee tb = _db.tblEmployees.Where(e => e.EmployeeID == emp.EmployeeId).FirstOrDefault();
+            tb.Name = emp.Name;
+            tb.Age = emp.Age;
+            tb.State = emp.State;
+            tb.Country = emp.Country;
+            return _db.SaveChanges();
+        }
+
+        //Method for Deleting an Employee
+        public int Delete(int ID)
+        {
+            tblEmployee tb = _db.tblEmployees.Where(e => e.EmployeeID == ID).FirstOrDefault();
+            _db.tblEmployees.Remove(tb);
+            return _db.SaveChanges();
+        }
+    }
+}
